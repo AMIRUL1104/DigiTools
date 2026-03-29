95aa75ead3f178850a73f41509549a4dc87dee9f
Amirul <amirulislam9.e@gmail.com>
2026-03-29 21:55:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* use this 3d card for welcome amirul */}
+      <section className=" container mx-auto mt-10">
+        <span className="skeleton skeleton-text"> Project is loading... </span>
+      </section>
+    </>
+  );
+}
+
+export default App;
